b9296f3e9233dba385aa680eb8a70057867765d7
Lyuba <nuvotlyuba@gmail.com>
2024-08-22 17:57:52 +0300

    fix port

--- internal/transport/handler/handler.go
+++ internal/transport/handler/handler.go
@@ -19,17 +19,4 @@ func New(service service.Services) *Handler {
 type Handlers interface {
 	PostURL(w http.ResponseWriter, r *http.Request)
 	GetURL(w http.ResponseWriter, r *http.Request)
-	Handler(w http.ResponseWriter, r *http.Request)
-}
-
-func (h Handler) Handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		h.PostURL(w, r)
-	case http.MethodGet:
-		h.GetURL(w, r)
-	default:
-		http.Error(w, "Неверный метод", http.StatusBadRequest)
-		return
-	}
 }
