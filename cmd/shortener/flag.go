0ea63996b2b39b4b412c4a390f7a4293ebd56f7a
Lyuba <nuvotlyuba@gmail.com>
2024-09-02 21:35:32 +0300

    fix

--- cmd/shortener/flag.go
+++ cmd/shortener/flag.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/nuvotlyuba/study-go-yandex/config"
 	"github.com/nuvotlyuba/study-go-yandex/internal/utils"
@@ -12,6 +13,10 @@ func parseFlags() {
 	flag.StringVar(&config.BaseURL, "b", config.BaseURL, "Base URL host:port")
 	flag.StringVar(&config.FileStoragePath, "f", config.FileStoragePath, "File storage path")
 	flag.Parse()
+
+	if os.Getenv("FILE_STORAGE_PATH") != "" {
+		config.FileStoragePath = os.Getenv("FILE_STORAGE_PATH")
+	}
 
 	if config.FileStoragePath != "" {
 		utils.MakeDir(config.FileStoragePath)
